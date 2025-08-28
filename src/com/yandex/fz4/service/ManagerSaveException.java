ccc1c270362d16a9dbd20b698307ed5ab3118166
besporyadokk <rakizWasTaken@yandex.ru>
2025-08-28 14:28:13 +0400

    исправления CodeStyle

--- src/com/yandex/fz4/service/ManagerSaveException.java
+++ src/com/yandex/fz4/service/ManagerSaveException.java
@@ -1,7 +1,7 @@
 package com.yandex.fz4.service;
 
 public class ManagerSaveException extends RuntimeException {
-    public ManagerSaveException(String message){
+    public ManagerSaveException(String message) {
         super(message);
     }
 }
