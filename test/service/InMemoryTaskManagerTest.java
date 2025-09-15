c2e25580dd0b07f663c78713c03db454d6ce7591
besporyadokk <rakizWasTaken@yandex.ru>
2025-09-15 23:58:54 +0300

    исправления для проверки GitHub

--- test/service/InMemoryTaskManagerTest.java
+++ test/service/InMemoryTaskManagerTest.java
@@ -12,6 +12,7 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InMemoryTaskManagerTest extends TaskManagerTest<InMemoryTaskManager> {
+    private TaskManager manager;
 
     @Override
     protected InMemoryTaskManager createTaskManager() {
