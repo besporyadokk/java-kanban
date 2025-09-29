8dcd68fc5d11ec73a02e18c542df18f6ec56e8f5
besporyadokk <rakizWasTaken@yandex.ru>
2025-09-29 22:23:54 +0300

    исправления тестов Git

--- test/http/BaseHttpTest.java
+++ test/http/BaseHttpTest.java
@@ -1,13 +1,16 @@
 package test.http;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.yandex.fz4.http.HttpTaskServer;
 import com.yandex.fz4.service.Managers;
 import com.yandex.fz4.service.TaskManager;
+import com.yandex.fz4.util.LocalDateTimeAdapter;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 public class BaseHttpTest {
     protected TaskManager taskManager;
@@ -18,7 +21,10 @@ public class BaseHttpTest {
     public void setUp() throws IOException {
         taskManager = Managers.getDefault();
         taskServer = new HttpTaskServer(taskManager);
-        gson = new Gson();
+
+        gson = new GsonBuilder()
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .create();
         taskServer.start();
     }
 
